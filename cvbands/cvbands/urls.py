bac583de6803a0f988835dd6d0c1f232bb65497a
Kenny Fryar-Ludwig <fryarludwig@gmail.com>
2015-12-16 12:05:31 -0700

    Initial url commits, just starting the Views folder

--- cvbands/cvbands/urls.py
+++ cvbands/cvbands/urls.py
@@ -19,4 +19,20 @@ from django.conf.urls import include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^auth/$', auth, name='auth'),
+    url(r'^$', MainPage.as_view(), name='home'),
+
+
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^auth/$', auth, name='auth'),
+    url(r'^submit_link/$', submit_link, name='submit_link'),
+    url(r'^logout/$', logout_view, name='logout'),
+    url(r'^register/$', register, name='register'),
+    url(r'^accounts/register/$', register, name='register'),
+    url(r'^r/$', subreddit_view, name='subreddits'),
+    url(r'^r/(?P<sub>[\w]+)/$', subreddit_selector, name='subreddits'),
+    url(r'^discuss/(?P<link>[\w-]+)/$', link_discuss, name='links'),
+    url(r'^v/l/(?P<link_id>[\w-]+)/$', vote_link, name='links'),
+    url(r'^v/c/(?P<comment_id>[\w-]+)/$', vote_comment, name='links'),
 ]
---
+++ cvbands/cvbands/views/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+from django.views.generic import ListView
+from django.template.defaultfilters import slugify
+from .models import Band, Event, Graphic, Video, Song, Album
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404
+from django.views.generic.edit import FormView
+from django.http import HttpResponse
+
+
+def auth(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('/')
+        else:
+            return redirect('/login')
+    else:
+        return redirect('/login')
+
+
+class MainPage(request):
+    pass
